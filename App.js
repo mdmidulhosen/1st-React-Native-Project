4cc4f60dd8976cd03d68f93c81ddb6d32189e95a
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-04-04 18:06:49 +0600

    some testing

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import {View, Text, TextInput,StyleSheet } from 'react-native'
+import React from 'react';
+import {View, Text, Image, TextInput,StyleSheet, Button, SafeAreaView, TouchableOpacity} from 'react-native';
+import Header from './component/Header';
 
 export default function Midul() {
+  handlePress = () => console.log("Clicked")
   return (
-    <View style={styles.container}> 
-      <Text style={{color:'red',}}>Hey There</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{color:'red',borderColor: 'red',}} numberOfLines={1} onPress={handlePress}>Hey There, I am Muhammad Mridul Hosen Kibria, my age is 20 and i am student and working on a it company name, Innova Infosys</Text>
       <TextInput style={{borderColor: 'red'}}></TextInput>
-    </View>
+      <TouchableOpacity onPress={()=> console.log("Image tapped")}>
+        <Image
+        fadeDuration={1000}
+        source={require("./assets/imgaes/cat.jpg")} style={styles.img}
+        /> 
+      </TouchableOpacity>
+      
+      <Header title2='Hey buddy'/>
+
+      <Button
+      title='Click Me' onPress={() => alert("Button Clicked")}>
+      </Button>
+    </SafeAreaView>
+
   )
 }
 
@@ -15,5 +30,10 @@ const styles = StyleSheet.create({
     flex: 1, 
     justifyContent: 'center', 
     alignItems: 'center',
+  },
+  img:{
+    width: 200,
+    height:200,  
+    borderRadius: 100,
   }
 })
---
+++ component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function Header(props) {
+  return (
+    <View>
+        <Text>{props.title2}</Text>
+    </View>
+  )
+}
